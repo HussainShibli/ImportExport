294eb4cc74f6892d8df38f1aec1ea94efb8bf368
HussainShibli <hussain.shibli@gmail.com>
2025-05-22 09:42:19 +0300

    Update app.py

--- app.py
+++ app.py
@@ -65,7 +65,7 @@ def render_combined_stacked_bar(df, metric, hs_level, show="both", selected_year
             xaxis_title="Year / Flow",
             yaxis_title=f"{metric} ({'USD' if metric == 'value' else 'kg'})",
             legend=dict(orientation='v', y=1, x=1.02, traceorder='normal'),
-            margin=dict(b=120)",
+            margin=dict(b=120),
             height=500,
             xaxis={'type': 'category', 'categoryorder': 'array', 'categoryarray': category_order}
         )
